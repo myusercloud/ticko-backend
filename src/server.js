7a0084a31b3573ebec25b881b2185608c2ed875a
myusercloud <lucasharrizon@gmail.com>
2026-03-13 02:47:41 +0300

    update environment variables for development setup

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const { env } = require('./config/env');
 const logger = require('./utils/logger');
 
-const PORT = env.PORT || 4000;
+const PORT = env.PORT || 5000;
 
 app.listen(PORT, () => {
   logger.info(`Server listening on port ${PORT}`);
